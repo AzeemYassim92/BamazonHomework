a1a8cb4325942ad946d5138d5cef0d2def79cb85
unknown <Azeem Yassim>
2018-11-26 12:44:50 -0500

    workingBamazon

---
+++ seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO products (product, department, price, stock)
+VALUES
+  ("Dell Laptop", "Electronics", 999, 50),
+  ("Jordans", "Clothing", 60, 25),
+  ("Harry Potter", "Books", 10, 35),
+  ("Get Rich or Die Trying", "Art", "Music", 10, 15),
+  ("Toaster", "Appliances", 399, 5);
